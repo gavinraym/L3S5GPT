f884bf39f15ae418aa117fad45d832ef7c835041
Gavin Ray <gavinraym@gmail.com>
2023-05-01 21:18:16 -0700

    Initial commit
    Adding two course tracks, python and web dev, with 2 puzzles and instructions for each.

---
+++ Python Track/read_first.py
@@ -0,0 +1,27 @@
+# Python has two different ways to write comments.
+
+# The first way is to use the hash character (#) at the beginning of a line.
+# The second way is to use triple quotes (""") at the beginning and end of a block of text.
+
+# In these puzzle files, I will use both forms of comments, but in different ways.
+# In programming, we would call this a "convention".
+
+# When you see a comment like this one, that is preceeded by a hash character (#),
+# you will know that it is instructions that you should read and follow.
+
+# Comments that are preceeded by triple quotes (""") are meant to be copy/pasted into
+# ChatGPT. Do this now with the next comment. Quotes can be included or not.
+
+"""What is a programming convention?"""
+
+# At the beginning of every puzzle, you will see a comment like this one. It will
+# contain a starter prompt to get you started. You will copy/paste this prompt into
+# ChatGPT and then copy/paste the code that ChatGPT generates into the puzzle file.
+# After that, it is up to you to decide what direction to take your program in.
+# Use your imagination and have fun!
+
+# Now you are ready to start the first puzzle! 
+
+# In the same directory as this file, you will find a file called "1.hello_world.py".
+# Open that file and read the instructions there.
+
